066a892c3167c545f5efc0a82887e8a871fab41a
Gema <gem.anggada@gmail.com>
2020-11-06 17:33:31 +0700

    feat(types): updated types def

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,10 @@
-import Ketikin, {Props} from './Ketikin';
-export default Ketikin;
+import { ReactNode } from "react";
+
+export type Props = {
+  children: (value: string) => ReactNode;
+  text: string;
+  texts?: string[];
+  interval?: number;
+};
+
+export default function Ketikin(props: Props): React.ReactNode;
